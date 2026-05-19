e86eb9e20c8fafedaf20632a57f18b31397bd403
jbleyel <jbleyel@gmx.net>
2026-05-19 16:53:25 +0200

    translation update

--- src/MovieManager/__init__.py
+++ src/MovieManager/__init__.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
-from os import environ as os_environ
 import gettext
+
+PluginLanguageDomain = "MovieManager"
 
 
 def localeInit():
 	localedir = resolveFilename(SCOPE_PLUGINS, "Extensions/MovieManager/locale")
-	gettext.bindtextdomain('MovieManager', localedir)
+	gettext.bindtextdomain(PluginLanguageDomain, localedir)
 
 
 def _(txt):
-	t = gettext.dgettext("MovieManager", txt)
+	t = gettext.dgettext(PluginLanguageDomain, txt)
 	if t == txt:
 		t = gettext.gettext(txt)
 	return t
 
 
 def ngettext(singular, plural, n):
-	t = gettext.dngettext('MovieManager', singular, plural, n)
+	t = gettext.dngettext(PluginLanguageDomain, singular, plural, n)
 	if t in (singular, plural):
 		t = gettext.ngettext(singular, plural, n)
 	return t
